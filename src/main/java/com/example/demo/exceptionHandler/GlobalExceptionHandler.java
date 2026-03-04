bc2a3980659f63baa1cfd98cd057651e6b2ef512
BaelNguyen <amn1022004@gmail.com>
2026-03-04 16:56:00 +0700

    handle exception

---
+++ src/main/java/com/example/demo/exceptionHandler/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.example.demo.exceptionHandler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handle(Exception e) {
+        e.printStackTrace();
+        return ResponseEntity.status(500).body(e.getMessage());
+    }
+}
